6979a9918feb95e72eb4ebc489131c3b13d9e2a0
tejakattenborn <38572828+tejakattenborn@users.noreply.github.com>
2021-12-17 20:05:59 +0100

    Update 0_average_plot_distance_v1.R

--- code/0_average_plot_distance_v1.R
+++ code/0_average_plot_distance_v1.R
@@ -1,14 +1,21 @@
 
+
+############## Description
+
+# Calculate the distance among the individual sites.
+# Manuscript: Kattenborn et al. > Spatially autocorrelated training and validation samples inflate performance assessment of convolutional neural networks
+# teja dot kattenborn at uni uni minus leipzig dot de
+
+
+############## Code
+
+
 require(rgdal)
 
-setwd("D:/googledrive/paper/paper_cnn_spatial_autocorrelation/plots_statistics")
-
-
+setwd("PATH")
 
 AOI = readOGR(dsn = getwd(), layer = "ConFoBio_plots")
-
 AOI = AOI[!AOI$plot_id==0,] # remove hainich data
-
 
 dist_matrix = dist(cbind(AOI$x, AOI$y))
 mean(dist_matrix)/1000 # mean in km
