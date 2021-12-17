640c3d340a5eb97792367adfaa3bed19c6e93b9b
tejakattenborn <38572828+tejakattenborn@users.noreply.github.com>
2021-12-17 20:08:44 +0100

    Update 2_correlogram_01_predictors_3.R

--- code/2_correlogram_01_predictors_3.R
+++ code/2_correlogram_01_predictors_3.R
@@ -1,152 +1,46 @@
 
 
+############## Description
+
+# calculate correlogram for the predictor variables based on the latent space (derived from the variational autoencoder)
+# Manuscript: Kattenborn et al. > Spatially autocorrelated training and validation samples inflate performance assessment of convolutional neural networks
+# teja dot kattenborn at uni uni minus leipzig dot de
+
+
+############## Code
+
 
 require(lattice)
 require(ncf)
-require(roll)
 
-wdir = "D:/data_ortho_confobi_sac"
+wdir = "PATH"
 setwd(wdir)
 
-range01 <- function(x){(x-mean(x))/(max(x[1:100])-mean(x))}
+meta_files = list.files("1_results_256_200lv_250epoch_latent/",full.names = T, pattern = "CFB", recursive = T)
 
+rm(meta_all)
+for (i in 1:length(meta_files)){
+  if(exists("meta_all")==F){
+    meta_all = read.csv(meta_files[i])
+    meta_all$X.1 = i
+  }else{
+    meta_all_tmp = read.csv(meta_files[i])
+    meta_all_tmp$X.1 = i
+    meta_all = rbind(meta_all,meta_all_tmp)
+  }
+}
 
-load(file = "correlog_output_copred_v2.RData")
-load(file = "correlog_output_coresp_resp_v2.RData")
+meta_all = as.data.frame(meta_all)
+meta_all$xmin = rowMeans(cbind(meta_all$xmax, meta_all$xmin))
+meta_all$ymin = rowMeans(cbind(meta_all$ymax, meta_all$ymin))
+meta_all = meta_all[, !(colnames(meta_all) %in% c("xmax","ymax", "X"))]
+colnames(meta_all)[1:3] = c("plot_id", "x", "y")
 
-# coresp = data.frame(dist = coresp$mean.of.class, correlation = coresp$correlation)[coresp$n > 2000,]
-# cotest = data.frame(dist = cotest$mean.of.class, correlation = cotest$correlation)[cotest$n > 2000,]
-# 
-# 
+### subsetting / settings
+dtest = meta_all[sample(1:nrow(meta_all), 20000),]
 
+########################################################
+### Correlogram
 
-cotest$correlation = range01(cotest$correlation)
-cotest_rmean = roll_mean(cotest$correlation[200:length(cotest$correlation)], width = 10000) #, min_obs = 1000)
-cotest_rsd = roll_sd(cotest$correlation[200:length(cotest$correlation)], width = 1000, min_obs = 1, center = TRUE)
-
-coresp$correlation = range01(coresp$correlation)
-coresp_rmean = roll_mean(coresp$correlation[200:length(coresp$correlation)], width = 10000) #, min_obs = 1000)
-coresp_rsd = roll_sd(coresp$correlation[200:length(coresp$correlation)], width = 1000, min_obs = 1, center = TRUE)
-
-
-
-col_respl = rgb(0, 100, 255, max = 255, alpha = 200)
-col_respp = rgb(0, 100, 255, max = 255, alpha = 50)
-col_testl = rgb(180, 0, 40, max = 255, alpha = 200)
-col_testp = rgb(180, 0, 40, max = 255, alpha = 50)
-
-
-
-pdf("correlogram_pred_resp_in_one_v3.pdf", width=8, height=4.5)
-
-
-par(mfrow=c(1,2))
-plot(cotest$correlation[1:100], type="l", lwd = 4, col = col_testl,
-     main = "a) SAC within orthoimages",
-     xlab = "distance [m]", ylab = "rel. multivariate correlation",
-     ylim=c(-0.7,1))
-grid(lty=1)
-lines(cotest$correlation[1:100], lwd = 4, col = col_testl)
-lines(coresp$correlation[1:100], lwd = 2, col=col_respl,
-      ylim = c(-0.7,1))
-
-
-legend("bottomleft", c("image tiles (predictors)", "species cover (response)") , lwd = 4, col=c(col_testl, col_respl), bty = "n")
-
-dftest <- data.frame(x =1:length(cotest_rmean),
-                 F =cotest_rmean,
-                 L =cotest_rmean-cotest_rsd,
-                 U =cotest_rmean+cotest_rsd)
-plot(dftest$x, dftest$F, ylim = c(-0.7,1), type = "l", yaxt = "n", ylab="", col=col_testl,
-     main = "b) SAC between orthoimages",
-     xlim = c(11000,70500))
-polygon(c(dftest$x,rev(dftest$x)),c(dftest$L,rev(dftest$U)),col = col_testp, border = FALSE)
-
-grid(lty=1)
-
-
-
-
-dfresp <- data.frame(x =1:length(coresp_rmean),
-                 F =coresp_rmean,
-                 L =coresp_rmean-coresp_rsd,
-                 U =coresp_rmean+coresp_rsd)
-
-polygon(c(dfresp$x,rev(dfresp$x)),c(dfresp$L,rev(dfresp$U)),col = col_respp, border = FALSE)
-lines(dfresp$x, dfresp$F, ylim = c(-0.7,1), type = "l", col=col_respl, lwd = 2,
-      xlim = c(11000,70500))
-lines(dftest$x, dftest$F, lwd = 4, col=col_testl)
-
-dev.off()
-
-
-
-
-
-
-
-require(zoo)
-
-
-testzoo = rollmean(cotest$correlation[1:length(cotest$correlation)], k=11, na.pad = T, fill = NA)
-testroll = roll_mean(cotest$correlation[1:length(cotest$correlation)], width = 10, complete_obs = F) #, min_obs = 1000)
-testrollapply = rollapply(cotest$correlation[1:length(cotest$correlation)], width = 10, FUN = mean, na.rm=T, partial=TRUE, align = "right", fill = NA)
-
-
-length(testzoo)
-length(testroll)
-length(testrollapply)
-
-plot(1:100,testzoo[1:100], ylim=c(0,0.8))
-points(1:100,testroll[1:100], col = "red")
-points(1:100,testrollapply[1:100], col="green")
-
-cotest$correlation = range01(cotest$correlation)
-cotest_rmean = testrollapply = rollapply(cotest$correlation[1:length(cotest$correlation)], width = 10000, FUN = mean, na.rm=T, partial=TRUE, align = "right", fill = NA)
-cotest_rsd = testrollapply = rollapply(cotest$correlation[1:length(cotest$correlation)], width = 1000, FUN = sd, na.rm=T, partial=TRUE, align = "right", fill = NA)
-
-
-coresp$correlation = range01(coresp$correlation)
-coresp_rmean = testrollapply = rollapply(coresp$correlation[1:length(coresp$correlation)], width = 10000, FUN = mean, na.rm=T, partial=TRUE, align = "right", fill = NA)
-coresp_rsd = testrollapply = rollapply(coresp$correlation[1:length(coresp$correlation)], width = 1000, FUN = sd, na.rm=T, partial=TRUE, align = "right", fill = NA)
-
-
-
-dftest <- data.frame(x =1:length(cotest_rmean),
-                     F =cotest_rmean,
-                     L =cotest_rmean-cotest_rsd,
-                     U =cotest_rmean+cotest_rsd)
-
-dfresp <- data.frame(x =1:length(coresp_rmean),
-                     F =coresp_rmean,
-                     L =coresp_rmean-coresp_rsd,
-                     U =coresp_rmean+coresp_rsd)
-
-
-dftest$L[1:100]
-dftest$U[1:100]
-dftest$F[1:100]
-
-dfresp$L[1:100]
-dfresp$U[1:100]
-dfresp$F[1:100]
-
-#testing only
-plot(cotest$correlation[1:100], type="l", lwd = 4, col = col_testl,
-     main = "a) SAC within orthoimages",
-     xlab = "distance [m]", ylab = "rel. multivariate correlation",
-     ylim=c(-0.7,1))
-grid(lty=1)
-lines(cotest$correlation[1:100], lwd = 4, col = col_testl)
-lines(coresp$correlation[1:100], lwd = 2, col=col_respl,
-      ylim = c(-0.7,1))
-
-polygon(c(dftest$x[1:100],rev(dftest$x[1:100])),c(dftest$L[1:100],rev(dftest$U[1:100])),col = col_testp, border = FALSE)
-
-
-
-
-plot(dftest$x, dftest$F, ylim = c(-0.7,1), type = "l", yaxt = "n", ylab="", col=col_testl,
-     main = "b) SAC between orthoimages",
-     xlim = c(11000,70500))
-polygon(c(dftest$x,rev(dftest$x)),c(dftest$L,rev(dftest$U)),col = col_testp, border = FALSE)
+cotest <- correlog(x = dtest$x, y = dtest$y, z = dtest[,4:ncol(dtest)], increment=1, resamp=10) # check autocorrleation, increment ~ bin size
+save(cotest, file = "correlog_output_copred_v2.RData")
