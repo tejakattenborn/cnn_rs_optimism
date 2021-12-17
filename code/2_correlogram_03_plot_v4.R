71ea8f7e607916e9b275d91d2b7f87ece570d22b
tejakattenborn <38572828+tejakattenborn@users.noreply.github.com>
2021-12-17 20:09:08 +0100

    Update 2_correlogram_03_plot_v4.R

--- code/2_correlogram_03_plot_v4.R
+++ code/2_correlogram_03_plot_v4.R
@@ -1,272 +1,73 @@
+
+
+############## Description
+
+# plot the correlograms of predictor and response variables within and across orthoimages
+# Manuscript: Kattenborn et al. > Spatially autocorrelated training and validation samples inflate performance assessment of convolutional neural networks
+# teja dot kattenborn at uni uni minus leipzig dot de
+
+
+############## Code
+
+
 require(lattice)
 require(ncf)
+require(roll)
+require(zoo)
 
-wdir = "D:/data_ortho_confobi_sac"
+wdir = "PATH"
 setwd(wdir)
 
-meta_files = list.files("1_results_256_200lv_250epoch_latent/",full.names = T, pattern = "CFB", recursive = T)
-
-rm(meta_all)
-for (i in 1:length(meta_files)){
-  if(exists("meta_all")==F){
-    meta_all = read.csv(meta_files[i])
-    meta_all$X.1 = i
-  }else{
-    meta_all_tmp = read.csv(meta_files[i])
-    meta_all_tmp$X.1 = i
-    meta_all = rbind(meta_all,meta_all_tmp)
-  }
-}
-
-meta_all = as.data.frame(meta_all)
-meta_all$xmin = rowMeans(cbind(meta_all$xmax, meta_all$xmin))
-meta_all$ymin = rowMeans(cbind(meta_all$ymax, meta_all$ymin))
-meta_all = meta_all[, !(colnames(meta_all) %in% c("xmax","ymax", "X"))]
-colnames(meta_all)[1:3] = c("plot_id", "x", "y")
+range01 <- function(x){(x-mean(x))/(max(x[1:100])-mean(x))}
 
 
-#write.csv(meta_all, "1_results_latentvariables/meta_all.csv", row.names = FALSE)
-
-
-
-### subsetting / settings
-dtest = meta_all[sample(1:nrow(meta_all), 20000),]
-#dtest = meta_all[meta_all$plot_id==c(plot),]  # plots where different varilog estimators deviate strongly: 1, 12 29, 30
-
-
-########################################################
-### Correlogram
-
-# test spline.correlog?
-par(mfrow = c(3,1))
-
-cotest <- correlog(x = dtest$x, y = dtest$y, z = dtest[,4:ncol(dtest)], increment=1, resamp=10) # check autocorrleation, increment ~ bin size
-
-#save(cotest, file = "correlog_output_copred_v2.RData")
 load(file = "correlog_output_copred_v2.RData")
+load(file = "correlog_output_coresp_resp_v2.RData")
 
-plot(cotest$mean.of.class, cotest$correlation, type = "p", ylim = c(-0.1,0.15), # plot correlogram
-     ylab="Moran Similarity", xlab="averaged distance class", main="a) Corrlog: all bins", pch=20)
-abline(h=0, col="red")
-#lines(cotest$mean.of.class,cotest$p-0.2, col="grey")
+cotest$correlation = range01(cotest$correlation)
+cotest_rmean = roll_mean(cotest$correlation[200:length(cotest$correlation)], width = 10000) #, min_obs = 1000)
+cotest_rsd = roll_sd(cotest$correlation[200:length(cotest$correlation)], width = 1000, min_obs = 1, center = TRUE)
 
-plot(cotest$mean.of.class[cotest$n>5000], cotest$correlation[cotest$n>5000], type = "p", ylim = c(-0.1,0.15), # plot correlogram
-     ylab="Moran Similarity", xlab="averaged distance class", main="b) Corrlog: bins with No. points > 5000", pch=20)
-abline(h=0, col="red")
+coresp$correlation = range01(coresp$correlation)
+coresp_rmean = roll_mean(coresp$correlation[200:length(coresp$correlation)], width = 10000) #, min_obs = 1000)
+coresp_rsd = roll_sd(coresp$correlation[200:length(coresp$correlation)], width = 1000, min_obs = 1, center = TRUE)
 
-plot(cotest$mean.of.class[cotest$n>5000], cotest$correlation[cotest$n>5000], type = "p", ylim = c(-0.1,0.15), # plot correlogram
-     ylab="Moran Similarity", xlab="averaged distance class", main="c) Corrlog: bins with No. points > 5000, log scale", log="x", pch=20)
-abline(h=0, col="red")
-
-
-# lines(cotest$mean.of.class, colMeans(cotest_allm, na.rm = T), lwd=2, col="red")
-# lines(cotest$mean.of.class, apply(cotest_allm, 2, quantile, probs = 0.25, na.rm = T), lwd=2, col="orange")
-# lines(cotest$mean.of.class, apply(cotest_allm, 2, quantile, probs = 0.75, na.rm = T), lwd=2, col="orange")
-
-# similarity vs distance
-mean(cotest$correlation[cotest$mean.of.class>150])
-mean(cotest$correlation[cotest$mean.of.class<150])
-mean(cotest$correlation[cotest$mean.of.class<20])
-mean(cotest$correlation[cotest$mean.of.class<10])
-
-# compare plateaus (20-150 vs 150 to Inf)
-ttest = t.test(cotest$correlation[cotest$mean.of.class>20 & cotest$mean.of.class<150], cotest$correlation[cotest$mean.of.class>150])
-ttest$p.value
-ttest$statistic
+col_respl = rgb(0, 100, 255, max = 255, alpha = 200)
+col_respp = rgb(0, 100, 255, max = 255, alpha = 50)
+col_testl = rgb(180, 0, 40, max = 255, alpha = 200)
+col_testp = rgb(180, 0, 40, max = 255, alpha = 50)
 
 
+pdf("correlogram_pred_resp_in_one_v3.pdf", width=8, height=4.5)
 
-# for two plot figure
+par(mfrow=c(1,2))
+plot(cotest$correlation[1:100], type="l", lwd = 4, col = col_testl,
+     main = "a) SAC within orthoimages",
+     xlab = "distance [m]", ylab = "rel. multivariate correlation",
+     ylim=c(-0.7,1))
+grid(lty=1)
+lines(cotest$correlation[1:100], lwd = 4, col = col_testl)
+lines(coresp$correlation[1:100], lwd = 2, col=col_respl,
+      ylim = c(-0.7,1))
 
+legend("bottomleft", c("image tiles (predictors)", "species cover (response)") , lwd = 4, col=c(col_testl, col_respl), bty = "n")
 
-pdf("fig_sac_rgb_v1.pdf", width=8, height=5)
-par(mfrow = c(1,2))
-par(mar=c(5.1,4.1,4.1,0.1)) # bottom, left, top and right
+dftest <- data.frame(x =1:length(cotest_rmean),
+                     F =cotest_rmean,
+                     L =cotest_rmean-cotest_rsd,
+                     U =cotest_rmean+cotest_rsd)
+plot(dftest$x, dftest$F, ylim = c(-0.7,1), type = "l", yaxt = "n", ylab="", col=col_testl,
+     main = "b) SAC between orthoimages",
+     xlim = c(11000,70500))
+polygon(c(dftest$x,rev(dftest$x)),c(dftest$L,rev(dftest$U)),col = col_testp, border = FALSE)
 
-plot(cotest$mean.of.class[cotest$mean.of.class < 100], cotest$correlation[cotest$mean.of.class < 100], type = "p", log="",
-     ylim = c(-0.1,0.11), # plot correlogram
-     ylab="Multivariate correlation", xlab="averaged distance class [m]", bty="n")
-#grid()
-abline(h=mean(cotest$correlation[cotest$mean.of.class<150 & cotest$mean.of.class>20]), col="red", lwd=2, lty=2)
-#abline(h=mean(cotest$correlation[cotest$mean.of.class>150]), col="blue", lwd=2, lty=2)
-legend("bottomleft", legend = c("mean > 20 m & < 120 m", "mean > 150 m"),
-       lty = c(2,2), col = c("red", "blue"), lwd=c(2,2),
-       bty="n")
-
-par(mar=c(5.1,0.1,4.1,0.1)) 
-plot(cotest$mean.of.class[cotest$n>5000 & cotest$mean.of.class>150], cotest$correlation[cotest$n>5000 & cotest$mean.of.class>150],type = "p",
-     ylim = c(-0.1,0.11), # plot correlogram
-     ylab="", xlab="averaged distance class", bty="n", yaxt = "n")
-#abline(h=mean(cotest$correlation[cotest$mean.of.class<150 & cotest$mean.of.class>20]), col="red", lwd=2, lty=2)
-abline(h=mean(cotest$correlation[cotest$mean.of.class>150]), col="blue", lwd=2, lty=2)
-#grid()
+grid(lty=1)
+dfresp <- data.frame(x =1:length(coresp_rmean),
+                     F =coresp_rmean,
+                     L =coresp_rmean-coresp_rsd,
+                     U =coresp_rmean+coresp_rsd)
+polygon(c(dfresp$x,rev(dfresp$x)),c(dfresp$L,rev(dfresp$U)),col = col_respp, border = FALSE)
+lines(dfresp$x, dfresp$F, ylim = c(-0.7,1), type = "l", col=col_respl, lwd = 2,
+      xlim = c(11000,70500))
+lines(dftest$x, dftest$F, lwd = 4, col=col_testl)
 dev.off()
-#abline(h=0, col="red")
-#lines(cotest$mean.of.class,cotest$p-0.2, col="grey")
-
-
-#devtools::install_github("LKremer/ggpointdensity")
-library(ggpointdensity)
-library(ggplot2)
-library(viridis)
-require(ggpubr)
-
-cotest_df = data.frame(dist = cotest$mean.of.class, cor = cotest$correlation)
-cotest_df = cotest_df[cotest$n > 4000,]
-
-p1 = ggplot(data = cotest_df, mapping = aes(x = dist, y = cor)) +
-  geom_pointdensity(adjust = 1) +
-  xlim(0, 100) +
-  ylim(-0.1, 0.1) +
-  xlab("distance [m]") + 
-  ylab("Multivariate correlation") +
-  ggtitle(label = "SAC within UAV flights") +
-  theme_minimal() +
-  theme(legend.position = "none",
-        plot.title = element_text(hjust = 0.5)) +
-  scale_color_viridis(begin=0, end=0)
-
-p2 = ggplot(data = cotest_df, mapping = aes(x = dist, y = cor)) +
-  geom_pointdensity(adjust = 1.0) +
-  #xlim(150, max(cotest_df$dist)) +
-  xlim(150, 60000) +
-  ylim(-0.1, 0.1) +
-  theme_minimal() +
-  xlab("distance [m]") + 
-  ylab("") +
-  #ylab("Multivariate correlation") + 
-  ggtitle(label = "SAC accross UAV flights") +
-  scale_color_viridis() +
-  #geom_hline(yintercept = mean(cotest_df$cor[cotest_df$dist > 150])) + 
-  theme(axis.title.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank(),
-        plot.title = element_text(hjust = 0.5)
-        #, legend.position = "none"
-  )
-
-p_both = ggarrange(p1, p2,
-                   #labels = c("SAC", "noSAC"),
-                   ncol = 2, nrow = 1,
-                   widths = c(0.7,1))
-
-p_both
-
-pdf("fig_sac_ggplot.pdf",
-    width=7, height=2.8)
-p_both
-dev.off()
-
-
-# coefficent of var https://stackoverflow.com/questions/13195442/moving-variance-in-r
-
-
-par(mfrow = c(1,2))
-fit3 = spline.correlog(x = dtest$x, y = dtest$y, z = dtest[,4:ncol(dtest)], resamp=10, np=1000)
-plot(fit3, ylim=c(0-0.05, 0.1), xlim=c(0,100))
-plot(fit3, ylim=c(0-0.05, 0.1))
-
-
-
-########################################################
-### Variogram ###
-
-
-data_geoR = as.geodata(dtest, coords.col = 2:3, data.col = 4:ncol(dtest))
-# calc semi-variance
-dtmp = seq(0,100,1) # define distance intervals (bis jetzt teste ich v.a. < 150, eignetlich will ich auch zeigen, dass SAC zwischen Plots geringer ist als in Plots)
-bin <- variog(data_geoR, uvec = dtmp, max.dist = max(dtmp), estimator.type="classical")
-env <-variog.mc.env(data_geoR, obj.variog = bin)
-
-
-
-plot(bin, envelope = env, pch=21, bg="grey", xlab="Distance (m)", ylab="Semivariance", main = "Semivarigoram on selected variable", xlim=c(0,100))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#require(geoR)
-data_geoR = as.geodata(dtest, coords.col = 2:3, data.col = 4:ncol(dtest))
-# calc semi-variance
-dtmp = seq(0,100,1) # define distance intervals (bis jetzt teste ich v.a. < 150, eignetlich will ich auch zeigen, dass SAC zwischen Plots geringer ist als in Plots)
-bin <- variog(data_geoR, uvec = dtmp, max.dist = max(dtmp), estimator.type="classical")
-
-plot(bin$u, rowSums(bin$v), pch=21, bg="grey", xlab="Distance (m)", ylab="Semivariance",
-     main = "Variog estimator 'classical", xlim=c(0,max(dtmp)))
-#, ylim=c(min(bin$v), max(bin$v)))
-grid()
-
-bin <- variog(data_geoR, uvec = dtmp, max.dist = max(dtmp), estimator.type="modulus")
-# plot 1) latent variable in space and 2) semi-variance
-plot(bin$u, rowSums(bin$v), pch=21, bg="grey", xlab="Distance (m)", ylab="Semivariance",
-     main = "Variog estimator 'Hawkins and Cressie'", xlim=c(0,max(dtmp)))
-grid()
-#, ylim=c(min(bin$v), max(bin$v)))
-
-
-
-
-
-
-
-
-
-
-
-
-dtest = meta_all[meta_all$plot_id==c(plot),]
-# multivariate
-cotest <- correlog(x = dtest$x, y = dtest$y, z = dtest[, c(4:ncol(dtest))], increment=1, resamp=10) # check autocorrleation, increment ~ bin size
-plot(cotest$mean.of.class, cotest$correlation, type = "o", ylim = c(-0.5,0.5), # plot correlogram
-     ylab="Moran Similarity", xlab="averaged distance class")
-lines(cotest$mean.of.class,cotest$p-0.3)
-abline(h=0, col="red")
-grid()
-plot = plot+1
-
-#non centered
-cotestnc <- correlog.nc(x = dtest$x, y = dtest$y, z = dtest[, c(4:ncol(dtest))], increment=1, resamp=10) 
-plot(cotestnc$mean.of.class, cotestnc$correlation, type = "o", ylim = c(-1,1), # plot correlogram
-     ylab="Moran Similarity", xlab="averaged distance class")
-lines(cotestnc$mean.of.class,cotestnc$p-0.7)
-
-
-lv = 4
-cotest <- correlog(x = dtest$x, y = dtest$y, z = dtest[,lv], increment=0.02, resamp=1)
-cotest_all = list()
-for(i in 5:(ncol(meta_all))){
-  cotest_all[[i-4]] <- correlog(x = dtest$x, y = dtest$y, z = dtest[,i], increment=0.02, resamp=1)$correlation
-  print(i)
-}
-cotest_allm = do.call(rbind, cotest_all)
-
-plot(cotest$mean.of.class, cotest$correlation, type = "l", ylim = c(-1,1), # plot correlogram
-     ylab="Moran Similarity", xlab="averaged distance class")
-
-lines(cotest$mean.of.class, colMeans(cotest_allm, na.rm = T), lwd=2, col="red")
-lines(cotest$mean.of.class, apply(cotest_allm, 2, quantile, probs = 0.25, na.rm = T), lwd=2, col="orange")
-lines(cotest$mean.of.class, apply(cotest_allm, 2, quantile, probs = 0.75, na.rm = T), lwd=2, col="orange")
-abline(h=0, lty =2)
-
-
-
-
-
-
-
-
-
-
-
-
-
